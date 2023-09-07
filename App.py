a4703e11e84f7be66edfbe8e3329e155ee2f9d6c
jesussalazr5 <jesussalazr5@gmail.com>
2023-09-06 20:01:29 -0400

    Pagina de login e salvar senha e user

--- App.py
+++ App.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import session
+from flask import flash
+from flask import url_for
+from flask import redirect
 
 app = Flask("Meu app")
+app.config["SECRET_KEY"] = "pudim"
 
 # mock
 posts = [
@@ -19,3 +25,16 @@ def exibir_entradas():
 @app.route("/layout")
 def layout():
     return render_template("layout.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form["username"] == "admin" and request.form["password"] == "admin":
+            session["logado"] = True
+            flash("Bem vindo")
+            return redirect(url_for("exibir_entradas"))
+        else:
+            erro = "Usuario ou senha invalidos"
+    return render_template("login.html", erro=erro)
