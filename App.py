62fb20ea6c1b15eabaa5f3fbd4e3933592f1b844
jesussalazr5 <jesussalazr5@gmail.com>
2023-09-11 21:27:36 -0400

    Corrigindo igual profesor final

--- App.py
+++ App.py
@@ -8,6 +8,8 @@ from flask import redirect
 from posts import posts
 from flask import abort
 import sqlite3
+from flask import g
+
 
 app = Flask(__name__)  # antes estava meu app
 app.config["SECRET_KEY"] = "pudim"
@@ -15,16 +17,34 @@ app.config.from_object(__name__)
 
 DATABASE = "banco.db"
 
+
 def conectar():
     return sqlite3.connect(DATABASE)
+
+
+@app.before_request
+def before_request():
+    g.bd = conectar()
+
+
+@app.teardown_request
+def teardown_request(f):
+    g.bd.close()
+
 
 @app.route("/")
 def exibir_entradas():
     # entradas = posts[::-1]  # Mock das postagens
-    sql = 'SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC'
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
     resultado = g.bd.execute(sql)
+    entradas = []
 
-    return render_template("exibir_entradas.html", entradas=sql)
+    for titulo, texto, data_criacao in resultado.fetchall():
+        entradas.append(
+            {"titulo": titulo, "texto": texto, "data_criacao": data_criacao}
+        )
+
+    return render_template("exibir_entradas.html", entradas=entradas)
 
 
 @app.route("/layout")
@@ -54,17 +74,21 @@ def logout():
 
 @app.route("/inserir", methods=["POST"])
 def inserir_entrada():
-    if session["logado"]:
-        novo_post = {"titulo": request.form["titulo"], "texto": request.form["texto"]}
-        posts.append(novo_post)
-        flash("Post criado com sucesso!")
+    if not session["logado"]:
+        abort(401)
+    titulo = request.form.get("titulo")
+    texto = request.form.get("texto")
+    sql = "INSERT INTO posts (titulo, texto) values(?,?)"
+    g.bd.execute(sql, [titulo, texto])
+    g.bd.commit()
+    flash("Post criado com sucesso!")
     return redirect(url_for("exibir_entradas"))
 
 
-@app.route("/posts/<int:id>")
-def detalhe_entrada(id):
-    try:
-        entradas = posts[id - 1]
-        return render_template("detalhe_entrada.html", entradas=entradas)
-    except Exception:
-        return abort(404)
+# @app.route("/posts/<int:id>")
+# def detalhe_entrada(id):
+#     try:
+#         entradas = posts[id - 1]
+#         return render_template("detalhe_entrada.html", entradas=entradas)
+#     except Exception:
+#         return abort(404)
--- esquema.sql
+++ esquema.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
 id integer primary key autoincrement,
 titulo string not null,
-data_criacao timestamp not null default current_timestamp
+texto string not null,
+data_criacao timestamp null default current_timestamp
 
 
 
